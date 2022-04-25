c1c428490435658ce6ba783937afac343b96d666
fturdean <fturdean@sfu.ca>
2022-04-25 14:08:53 -0700

    add test for api response

---
+++ main.py
@@ -0,0 +1,22 @@
+import json
+from requests import Session
+
+f = open('keys.json')
+keys = json.load(f)
+TOKEN = keys['API_TOKEN']
+f.close()
+
+class AQICN_Parser:
+    def __init__(self, token):
+        self.api_url = 'https://api.waqi.info'
+        self.session = Session()
+        self.token = token
+
+    """ run query with parameters at API endpoint url, returns ok 200 if API query ok"""
+    def query_api(self, url, parameters):
+        pass
+    
+    """ returns a JSON object containing station information for all
+    stations with the coordinate string defined by crdnt_str """
+    def get_stations(self, crdnt_str):
+        pass
--- tests.py
+++ tests.py
@@ -1,1 +1,19 @@
 import unittest
+
+from main import TOKEN, AQICN_Parser
+
+
+
+class TestAQICN_Parser(unittest.TestCase):
+    def setUp(self):
+        self.parser = AQICN_Parser(TOKEN)
+    
+    def test_query_api_returns_ok(self):
+        crdnts = '39.379436,116.091230,40.235643,116.784382'
+        parameters = {'latlng': crdnts, 'token': self.parser.token}
+        url = 'https://api.waqi.info/v2/map/bounds'
+        r = self.parser.query_api(url, parameters)
+        self.assertEqual(r.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
